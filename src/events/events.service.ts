35f40f9b8c184f857d2450d2532907141d64a52c
Naveenkumar <pmnaveenmano@gmail.com>
2022-09-14 15:24:51 +0530

    Events, Workshop entities and service updated

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -92,7 +92,9 @@ export class EventsService {
 
   @Get('events')
   async getEventsWithWorkshops() {
-    throw new Error('TODO task 1');
+    return this.eventRepository.createQueryBuilder('event')
+                                       .leftJoinAndSelect('event.workshops', 'workshop')
+                                       .getMany();
   }
 
   /*
